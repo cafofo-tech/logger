844675d62ccc1b677d675cc6aef9c122b56a3395
Rafael Uchôa <uchoaaa@gmail.com>
2021-06-25 14:23:41 -0300

    Config type:module and add some colors

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+const COLORS = {
+  Reset: "\x1b[0m",
+  Bright: "\x1b[1m",
+  Dim: "\x1b[2m",
+  Underscore: "\x1b[4m",
+  Blink: "\x1b[5m",
+  Reverse: "\x1b[7m",
+  Hidden: "\x1b[8m",
+  FgBlack: "\x1b[30m",
+  FgRed: "\x1b[31m",
+  FgGreen: "\x1b[32m",
+  FgYellow: "\x1b[33m",
+  FgBlue: "\x1b[34m",
+  FgMagenta: "\x1b[35m",
+  FgCyan: "\x1b[36m",
+  FgWhite: "\x1b[37m",
+  BgBlack: "\x1b[40m",
+  BgRed: "\x1b[41m",
+  BgGreen: "\x1b[42m",
+  BgYellow: "\x1b[43m",
+  BgBlue: "\x1b[44m",
+  BgMagenta: "\x1b[45m",
+  BgCyan: "\x1b[46m",
+  BgWhite: "\x1b[47m"
+}
+
 export default class Logger {
   static _instance = null
 
@@ -35,21 +61,21 @@ export default class Logger {
 
   debug(...args) {
     if(this.level > Logger.DEBUG){ return }
-    this._doLog('debug', ...args)
+    this._doLog('debug', COLORS.Reset, ...args)
   }
 
   info(...args) {
     if(this.level > Logger.INFO){ return }
-    this._doLog('info', ...args)
+    this._doLog('info', COLORS.FgGreen, ...args)
   }
 
   warn(...args) {
     if(this.level > Logger.WARN){ return }
-    this._doLog('warn', ...args)
+    this._doLog('warn', COLORS.FgYellow, ...args)
   }
 
   // private
-  _doLog(level, ...args) {
-    console[level](`[${level.toUpperCase()}] ${new Date().toISOString()} - (${this.contextName}):`, ...args)
+  _doLog(level, color, ...args) {
+    console[level](`${color}[${level.toUpperCase()}] ${new Date().toISOString()} - (${this.contextName}):`, ...args)
   }
 }
